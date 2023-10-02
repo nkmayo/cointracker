67f822e32b9fa23a3c60049b2d3318b8a6c05cee
nkmayo <nkmayo@gmail.com>
2023-10-02 15:14:16 -0500

    Paths now used vs strings

--- cointracker/run.py
+++ cointracker/run.py
@@ -1,70 +1,104 @@
-# %% 
+# %%
 # make sure you are in the correct directory . and not ./cointracker
-from cointracker.util.util import getEmptyPurchasePool, getEmptySalePool, executeOrder, prettyPrintPools
+from util.util import (
+    getEmptyPurchasePool,
+    getEmptySalePool,
+    executeOrder,
+    prettyPrintPools,
+)
 import pandas as pd
 import datetime
+
 # import ctypes  # For popup windows
 import tkinter as tk
 from tkinter import filedialog
-from cointracker.util import orderBook as ob
+from util import orderBook as ob
+from pathlib import Path
 
 # %%
-if (__name__ == '__main__'):
+if __name__ == "__main__":
+    fromExcel = True
+    fromExisingPools = False
     root = tk.Tk()
     root.withdraw()
-    filename = filedialog.askopenfilename(title="Select current year's order book",
-        filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
-    # orderbook file name
-    outputPath = '../data/' # this used to work with './data', new file path?
-    # sheetname = '2021 OrderBook'
-    oBook = pd.read_csv(filename, parse_dates=['Date(UTC)'])
-    
-    startDate = datetime.datetime(2020, 1, 1, tzinfo=datetime.timezone.utc)
+
+    base_path = (Path(__file__).parents[1]).resolve()
+    outputPath = base_path / "data/"  # this used to work with './data', new file path?
+    if fromExcel:
+        filename = filedialog.askopenfilename(
+            title="Select current year's order book",
+            filetypes=(("Excel files", "*.xlsx"), ("all files", "*.*")),
+        )
+        sheetname = "Combined"
+
+        oBook = ob.parseOrderBook(filename, sheetname)
+        outputFilename = outputPath / "OrderBookRefined.csv"
+        oBook.to_csv(outputFilename, index=False, encoding="utf-8")
+    else:
+        filename = filedialog.askopenfilename(
+            title="Select current year's order book",
+            filetypes=(("CSV files", "*.csv"), ("all files", "*.*")),
+        )
+        oBook = pd.read_csv(filename, parse_dates=["Date(UTC)"])
+
+    startDate = datetime.datetime(2017, 1, 1, tzinfo=datetime.timezone.utc)
+    # startDate = datetime.datetime(2020, 1, 1, tzinfo=datetime.timezone.utc)
     # endDate = datetime.datetime.now()
-    endDate = datetime.datetime(2021, 1, 1, tzinfo=datetime.timezone.utc)
-    '''
-    oBook = ob.parseOrderBook(filename, sheetname)
-    outputFilename = outputPath + 'OrderBookRefined.csv'
-    oBook.to_csv(outputFilename, index=False, encoding='utf-8')
-    '''
+    endDate = datetime.datetime(2023, 1, 1, tzinfo=datetime.timezone.utc)
+    filingYear = [2022]  # list of years to include in short/long/etc pools reports
+
     # only consider orders throughout the outlined dates
-    oBook = oBook[(oBook['Date(UTC)'] >= startDate) &
-                  (oBook['Date(UTC)'] < endDate)]
-    
-    # load existing purchase pools
-    purchasePath = filedialog.askopenfilename(title="Select file of existing purchase pools",
-        filetypes=(("CSV files", "*.csv"), ("all files", "*.*")))
+    oBook = oBook[(oBook["Date(UTC)"] >= startDate) & (oBook["Date(UTC)"] < endDate)]
+    oBook = oBook.sort_values(by=["Date(UTC)", "Market", "Type"]).reset_index(drop=True)
 
-    try:
-        purchasePools = pd.read_csv(purchasePath, parse_dates=['Purchase Date'])
-        # purchasePools['Purchase Date'] = \  # already done with parse_dates
-        #    pd.to_datetime(purchasePools['Purchase Date'], utc=True)  # convert to datetime
-        purchasePools['Holding Period Modifier'] = \
-            pd.to_timedelta(purchasePools['Holding Period Modifier'])  # convert to timedelta
-        # print(purchasePools.dtypes)
-        # print(type(purchasePools.loc[1,'Asset']))
-    except Exception:
-        print("No purchase pool file read")
+    if fromExisingPools:
+        # NOTE: You have to load the previous purchase pools and sale pools, rather than just the asset pools
+        # if you are going to take care of potential wash sales taking place near the end/beginning of the fiscal period.
+        # load existing purchase pools
+        purchasePath = filedialog.askopenfilename(
+            title="Select file of existing purchase pools",
+            filetypes=(("CSV files", "*.csv"), ("all files", "*.*")),
+        )
+
+        try:
+            purchasePools = pd.read_csv(purchasePath, parse_dates=["Purchase Date"])
+            # purchasePools['Purchase Date'] = \  # already done with parse_dates
+            #    pd.to_datetime(purchasePools['Purchase Date'], utc=True)  # convert to datetime
+            purchasePools["Holding Period Modifier"] = pd.to_timedelta(
+                purchasePools["Holding Period Modifier"]
+            )  # convert to timedelta
+            # print(purchasePools.dtypes)
+            # print(type(purchasePools.loc[1,'Asset']))
+        except Exception:
+            print("No purchase pool file read")
+            purchasePools = getEmptyPurchasePool()
+
+        # load existing sale pools (to check for wash sales near previous year end boundary)
+        salePath = filedialog.askopenfilename(
+            title="Select file of existing sale pools",
+            filetypes=(("CSV files", "*.csv"), ("all files", "*.*")),
+        )
+
+        try:
+            salePools = pd.read_csv(
+                salePath, parse_dates=["Purchase Date", "Sale Date"]
+            )
+            # salePools['Purchase Date'] = \
+            #    pd.to_datetime(salePools['Purchase Date'], utc=True)  # convert to datetime
+            # salePools['Sale Date'] = \
+            #    pd.to_datetime(salePools['Sale Date'], utc=True)  # convert to datetime
+            salePools["Holding Period"] = pd.to_timedelta(
+                salePools["Holding Period"]
+            )  # convert to timedelta
+        except Exception:
+            print("No sale pool file read")
+            salePools = getEmptySalePool()
+
+    else:
         purchasePools = getEmptyPurchasePool()
-
-    # load existing sale pools (to check for wash sales near previous year end boundary)
-    salePath = filedialog.askopenfilename(title="Select file of existing sale pools",
-                                          filetypes=(("CSV files", "*.csv"),
-                                          ("all files", "*.*")))
-
-    try:
-        salePools = pd.read_csv(salePath, parse_dates=['Purchase Date', 'Sale Date'])
-        # salePools['Purchase Date'] = \
-        #    pd.to_datetime(salePools['Purchase Date'], utc=True)  # convert to datetime
-        # salePools['Sale Date'] = \
-        #    pd.to_datetime(salePools['Sale Date'], utc=True)  # convert to datetime
-        salePools['Holding Period'] = \
-            pd.to_timedelta(salePools['Holding Period'])  # convert to timedelta
-    except Exception:
-        print("No sale pool file read")
         salePools = getEmptySalePool()
 
-    invMethod = 'LIFO'
+    invMethod = "LIFO"
 
     # print(salePools['Long'])
     # print(list(salePools.columns.values))
@@ -75,29 +109,33 @@ if (__name__ == '__main__'):
         order = oBook.loc[index:index]
         order = order.reset_index()
 
-        temp = order.loc[0, 'Market']
+        temp = order.loc[0, "Market"]
         try:
-            asset1, asset2 = temp.split('-')
+            asset1, asset2 = temp.split("-")
         except Exception:
             asset1 = temp
-            asset2 = 'USD'
+            asset2 = "USD"
 
-        orderType = order.loc[0, 'Type']
+        orderType = order.loc[0, "Type"]
         oderType = orderType.upper()
         orderNum = orderNum + 1
 
-        print('Executing order #', orderNum)
-        print('Order Type/Market: ', orderType, '/', asset1, '-', asset2)
+        print("Executing order #", orderNum)
+        print("Order Type/Market: ", orderType, "/", asset1, "-", asset2)
 
-        purchasePools, salePools = executeOrder(order, purchasePools, salePools, invMethod)
+        purchasePools, salePools = executeOrder(
+            order, purchasePools, salePools, invMethod
+        )
 
     # organize the pools as necessary
-    purchasePools = purchasePools.sort_values(by=['Asset', 'Purchase Date'],
-                                              ascending=[True, False])
-    salePools = salePools.sort_values(by=['Asset Sold', 'Sale Date', 'Purchase Date'],
-                                      ascending=[True, True, False])
+    purchasePools = purchasePools.sort_values(
+        by=["Asset", "Purchase Date"], ascending=[True, False]
+    )
+    salePools = salePools.sort_values(
+        by=["Asset Sold", "Sale Date", "Purchase Date"], ascending=[True, True, False]
+    )
 
-    print('Finished')
+    print("Finished")
 
     # ----------------------PRINT OUTPUT TO FILES----------------------#
 
@@ -109,32 +147,28 @@ if (__name__ == '__main__'):
     # & (purchasePools['Asset']==asset)]
     # assetPools = potentialPools.sort_values(by=['Purchase Date'], ascending = True)
 
-    yearSalePools = salePools[(salePools['Sale Date'] >= startDate) &
-                              (salePools['Sale Date'] < endDate)]
-    shortPools = yearSalePools[~yearSalePools['Long']]
-    longPools = yearSalePools[yearSalePools['Long']]
-    assetPools = purchasePools[purchasePools['Active']]
+    yearSalePools = salePools[
+        pd.DatetimeIndex(salePools["Sale Date"]).year.isin(filingYear)
+    ]
+    shortPools = yearSalePools[yearSalePools["Long"] == False]
+    longPools = yearSalePools[yearSalePools["Long"] == True]
+    assetPools = purchasePools[purchasePools["Active"] == True]
 
     # output to files
-    outputFilename = outputPath + 'Puchase Pools.csv'
+    outputFilename = outputPath / "Puchase Pools.csv"
 
-    purchasePools.to_csv(outputFilename, index=False, encoding='utf-8')
+    purchasePools.to_csv(outputFilename, index=False, encoding="utf-8")
 
-    outputFilename = outputPath + 'Sale Pools.csv'
-    salePools.to_csv(outputFilename, index=False, encoding='utf-8')
+    outputFilename = outputPath / "Sale Pools.csv"
+    salePools.to_csv(outputFilename, index=False, encoding="utf-8")
 
-    outputFilename = outputPath + 'Asset Pools.csv'
-    assetPools.to_csv(outputFilename, index=False, encoding='utf-8')
+    outputFilename = outputPath / "Asset Pools.csv"
+    assetPools.to_csv(outputFilename, index=False, encoding="utf-8")
 
-    outputFilename = outputPath + 'Short Pools.csv'
+    outputFilename = outputPath / "Short Pools.csv"
     prettyPrintPools(shortPools, outputFilename)
 
-    outputFilename = outputPath + 'Long Pools.csv'
+    outputFilename = outputPath / "Long Pools.csv"
     prettyPrintPools(longPools, outputFilename)
 
-# %%
-df =  pd.DataFrame({'one': [5, 6, 7 , 8]})
-print(df)
-# %%
-df.to_csv('../data/testdf.csv')
 # %%
